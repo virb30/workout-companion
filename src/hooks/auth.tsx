9a00a8abca6e10b0a0597b74d8a88347733eee99
Vinicius Boscoa <virb30@gmail.com>
2020-05-22 19:00:51 -0300

    Initial commit

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,70 @@
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useCallback,
+} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+
+interface User {
+  name: string;
+  email: string;
+  weight: string;
+  height: string;
+}
+
+interface AuthState {
+  user: User;
+}
+
+interface AuthContexData {
+  user: User;
+  loading: boolean;
+  signIn(user: User): Promise<void>;
+}
+
+const AuthContext = createContext<AuthContexData>({} as AuthContexData);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadStoragedData(): Promise<void> {
+      const user = await AsyncStorage.getItem('@WorkoutCompanion:user');
+
+      if (user) {
+        setData({ user: JSON.parse(user) });
+      }
+
+      setLoading(false);
+    }
+
+    loadStoragedData();
+  }, []);
+
+  const signIn = useCallback(async user => {
+    await AsyncStorage.setItem('@WorkoutCompanion:user', JSON.stringify(user));
+
+    setData({ user });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, loading, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): AuthContexData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Dashboard from '../pages/Dashboard';
+
+const App = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+  <App.Navigator
+    screenOptions={{
+      headerTintColor: '#fff',
+      headerStyle: { backgroundColor: '#1a1818' },
+      cardStyle: { backgroundColor: '#383535' },
+    }}
+  >
+    <App.Screen name="Dashboard" component={Dashboard} />
+  </App.Navigator>
+);
+
+export default AppRoutes;
