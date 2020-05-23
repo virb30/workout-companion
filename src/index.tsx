968ab24a3f42ab8422c16c68b057aaa2737e9c15
Vinicius Boscoa <virb30@gmail.com>
2020-05-23 19:54:11 -0300

    finished watermelondb configuration

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import DatabaseProvider from '@nozbe/watermelondb/DatabaseProvider';
+
+import database from './database';
 
 import AppProvider from './hooks';
 
@@ -11,11 +14,13 @@ const App: React.FC = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#1a1818" />
-      <AppProvider>
-        <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-          <Routes />
-        </View>
-      </AppProvider>
+      <DatabaseProvider database={database}>
+        <AppProvider>
+          <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+            <Routes />
+          </View>
+        </AppProvider>
+      </DatabaseProvider>
     </NavigationContainer>
   );
 };
